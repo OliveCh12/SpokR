ae0a9dacccea5a265d62f4a641da7d2eb57d6b7d
Olivier <olivierchemla@gmail.com>
2019-05-04 23:56:26 +0200

    Updating App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Router, Route, Switch } from "react-router-dom";
 
-import './App.css'
+import "./App.css";
 
-import Header from './components/Header'
-// import Footer from './components/Footer'
-import Overview from './components/Dashboard/Overview'
+import Header from "./components/Header";
+import Overview from "./components/Dashboard/Overview";
+
+// Informations Components
+import Empty from "./components/Layout/Empty"
+import Welcome from "./components/Public/Welcome"
+
+// Auth Components
+import Signin from "./components/Auth/Signin"
+import Signup from "./components/Auth/Signup"
+
+
+import FirestoreCollection from "./components/Firestore"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header name="SpokR"/> 
-      <Overview />
-      {/* <Footer /> */}
+        <Header name="SpokR" />
+        <Switch>
+          {/* Public Routes */}
+          <Route path="/welcome" component={Welcome} />
+          <Route path="/signin" component={Signin} />
+          <Route path="/signup" component={Signup} />
+
+          {/* Protected Routes */}
+          <Route path="/stories" component={Overview} />
+          <Route path="/firestore" component={FirestoreCollection} />
+          <Route path="/explore" component={Overview} />
+          <Route component={Empty} />
+        </Switch>
       </div>
     );
   }
